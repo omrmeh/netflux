c9abddc3398e423731f520d14d1eb321a7c9ec7f
Siham <siham_mbs@hotmail.com>
2019-03-09 13:49:15 +0100

    upDate IHM: comboBox, Exit, Add, couleurs des lineEdit selon si affichage ou mod

--- NetfluxApp/moviecard.h
+++ NetfluxApp/moviecard.h
@@ -15,8 +15,18 @@ public:
     explicit MovieCard(QWidget *parent = nullptr);
     ~MovieCard();
 
+    void newCard();
+
 private:
     Ui::Form *ui;
+
+private slots:
+
+
+    void enabledCard();
+    void editMovie();
+    void saveMovie();
+
 };
 
 #endif // MOVIECARD_H
