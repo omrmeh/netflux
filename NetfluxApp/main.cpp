02909e39a5ccb319346042709b69bf12d41da334
omrmeh <omar.mehyaoui@gmail.com>
2019-03-14 10:17:18 +0100

    ajout du QSettings

--- NetfluxApp/main.cpp
+++ NetfluxApp/main.cpp
@@ -4,20 +4,43 @@
 #include <QSqlDatabase>
 #include <QDebug>
 #include <customwidget.h>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
+    /*
+     * configuration du fichier ini
+     */
+
+    QCoreApplication::setOrganizationName("Aelion");
+    QCoreApplication::setOrganizationDomain("https://github.com/omrmeh/netflux");
+    QCoreApplication::setApplicationName("Netflux");
+    QSettings settings;
+
+    settings.setValue("db/hostname", "localhost");
+    settings.setValue("db/name", "netflux");
+    settings.setValue("db/username", "cinemauser");
+    settings.setValue("db/password", "0000");
+    settings.setValue("db/port", 5432);
+
+    /*
+     *
+     */
+
     QApplication a(argc, argv);
 
     //Création d'un objet base de données
     QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL", "dbFilm");
-    db.setHostName("localhost");
-    db.setDatabaseName("netflux");
-    db.setUserName("postgres");
-    db.setPassword("postgres123");
-    db.setPort(5432);
+
+
+    db.setHostName(settings.value("db/hostname", "localhost"/*valeur par défaut si ini non trouvé*/).toString());
+    db.setDatabaseName(settings.value("db/name", "netflux").toString());
+    db.setUserName(settings.value("db/username", "cinemauser").toString());
+    db.setPassword(settings.value("db/password", "postgres123").toString());
+    db.setPort(settings.value("db/port",5432).toInt());
 
     qDebug() << db <<endl;
+
     bool ok = db.open();
 
             // si ok                    si !ok
